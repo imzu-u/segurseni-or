a153608d998d2d18beda4d1e8c0495721b58ba81
imzu-u <imzu-u@github.com>
2025-12-05 01:21:08 -0500

    SegurSenior - App de seguridad emocional

---
+++ deploy.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d "c:\Users\JHAIR\Desktop\html-01\hhh"
+git init
+git add .
+git commit -m "SegurSenior - App de seguridad emocional"
+git branch -M main
+git remote add origin https://github.com/imzu-u/segurseni-or.git
+git push -u origin main
+pause
